233ccb3fb10b27d1136f8d5e0a967a2380490fe7
Viren Laniya <virenlaniya.developer@gmail.com>
2026-06-28 09:34:09 +0530

    chore: remove unecessary mediatr register

--- Finervo.API/Shared/Extensions/ServiceCollectionExtensions.cs
+++ Finervo.API/Shared/Extensions/ServiceCollectionExtensions.cs
@@ -9,7 +9,6 @@ namespace Finervo.API.Shared.Extensions
         public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
         {
             services
-                .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly))
                 .AddContracts()
                 .AddApplication()
                 .AddInfrastructure(configuration)
