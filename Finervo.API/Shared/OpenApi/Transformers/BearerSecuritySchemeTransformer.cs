f49286744d949002fe932c9f3d9505249e6ec35e
Viren Laniya <virenlaniya.developer@gmail.com>
2026-08-19 00:46:47 +0530

    refactor: add bearer security scheme transformer and replaced with old document transformer

---
+++ Finervo.API/Shared/OpenApi/Transformers/BearerSecuritySchemeTransformer.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi;
+
+namespace Finervo.API.Shared.OpenApi.Transformers
+{
+    public sealed class BearerSecuritySchemeTransformer : IOpenApiDocumentTransformer
+    {
+        public Task TransformAsync(
+            OpenApiDocument document,
+            OpenApiDocumentTransformerContext context,
+            CancellationToken cancellationToken)
+        {
+            document.Components ??= new OpenApiComponents();
+
+            document.Components.SecuritySchemes = new Dictionary<string, IOpenApiSecurityScheme>
+        {
+            {
+                "Bearer",
+                new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Name = HeaderNames.Authorization,
+                    Description = "Enter your JWT access token. Example: eyJhbGci..."
+                }
+            }
+        };
+
+            var securityRequirement = new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecuritySchemeReference("Bearer", document),
+                new List<string>()
+            }
+        };
+
+            foreach (var path in document.Paths.Values)
+            {
+                if (path?.Operations is null)
+                    continue;
+
+                foreach (var operation in path.Operations.Values)
+                {
+                    operation.Security ??= [];
+                    operation.Security.Add(securityRequirement);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
